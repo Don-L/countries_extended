7830691b44cb50bbd5ca58fe3058bb04696185ae
Donald Lessells <donald.lessells@gmail.com>
2016-06-09 09:14:07 +0100

    got the basics working with help from keith and rick's code

--- public/map.js
+++ public/map.js
@@ -1,4 +1,4 @@
-var Map = function(latLng, zoom) {  //our map constructor
+var Map = function(latLng, zoom) { 
 
   this.googleMap = new google.maps.Map(document.getElementById('map'), {
     center: latLng,
@@ -21,22 +21,9 @@ var Map = function(latLng, zoom) {  //our map constructor
       position: latLng,
       label: '',
       map: this.googleMap,
-      title: title //need to call key 'title' so google maps can use the property. we could have a label key as well as a title key if we wanted
+      title: title 
     })
     return marker;
-  };
-
-  this.bindClick = function() {
-    google.maps.event.addListener(this.googleMap, 'click', function(event){ //click is an event type provided by google. we need to reference it by placing its name in a string
-      console.log(event);
-      console.log(event.latLng.lat());
-      console.log(event.latLng.lng());
-      this.addMarker({lat: event.latLng.lat(), lng: event.latLng.lng()}, 'X');
-    }.bind(this)); //binds 'this' in the bindclick method to Map
-  };
-
-  this.resetCenter = function(latLng) {
-    this.googleMap.panTo(latLng);
   };
 
 
